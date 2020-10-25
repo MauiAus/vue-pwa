d77c99b2eaeb1587953a481955a3b38cf0b68fa8
Pocholo <plpantoja49@gmail.com>
2020-10-25 17:47:07 +0800

    feat: fill up left sidebar

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,16 @@
     <div class="dashboard">
         <!-- Left Sidebar -->
         <div id="left">
-            Left
+            <div id="backpack">
+                Backpack
+            </div>
+            <a id="link" href="#">Home</a>
+            <a id="link" href="#">Library</a>
+            <a id="link" href="#">Create Study Set</a>
+            <a id="link" href="#">Settings</a>
+            <a id="logout" href="#">
+                Logout
+            </a>
         </div>
         <!-- Main Content -->
         <div id="mid">
@@ -34,6 +43,7 @@ export default {
     // the left and right sidebar will share these properties
     // we will simply call this selector via @extend
     position: fixed;
+    text-decoration: none;
     height: 100%;
     z-index: 1;
     top: 0;
@@ -45,6 +55,43 @@ export default {
     width: 20%;
     left: 0;
     background-color: #fbf9fa;
+
+    /* Trademark */
+    #backpack {
+        padding: 10% 0%;
+        font-size: 4vh;
+        font-weight: 700;
+        text-align: center;
+        color: #84ccc9;
+    }
+
+    /* Sidebar Links */
+    #link {
+        margin: 5% 20%;
+        display: block;
+        font-size: 16px;
+        color: #a5a5a5;
+
+        &:hover {
+            color: #84ccc9;
+        }
+    }
+
+    /* Logout segment */
+    #logout {
+        position: absolute;
+        display: block;
+        bottom: 0;
+        color: #fff;
+        background-color: #ff8080;
+        padding: 5% 0%;
+        width: 100%;
+        text-align: center;
+
+        &:hover {
+            background-color: #ff5c5c;
+        }
+    }
 }
 
 /* Mid */
