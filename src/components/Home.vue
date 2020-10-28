37e4de490f6ef96279fcdb4c400d15e24ef822ad
Joshinobi <gonzalesjosh1999@gmail.com>
2020-10-28 15:10:55 +0800

    Search bar update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
+    
     <div class="home">
+        <!-- Search bar -->
+        <div class="search-box">
+            <a class="search-btn" href="#">
+                <i class="fas fa-search fa-lg"></i>
+            </a>
+            <input class="search-txt" type="text" name="" placeholder="Search">
+            <a class="caret-down-btn" href="#">
+                <i class="fas fa-caret-down fa-lg"></i>
+            </a>
+        </div>
+
+        <!-- Home content -->
         <div id="welcome-box">
             <div id="left">
                 <p>Welcome Back Juan!</p>
@@ -31,7 +44,56 @@ export default {
     padding: 3.75%;
 }
 
+/*Search bar styles*/
+.search-box{
+    margin-top: 0%;
+    margin-bottom: 2%;
+    left: 50%;
+    background: #e1e3e2;
+    height: 24px;
+    border-radius: 40px;
+    padding: 10px;
+}
+
+.search-box:hover{
+    background: #b9e8d4;
+}
+
+.search-btn{
+    color: #52c3a5;
+    margin-left: 10px;
+    margin-right: 20px;
+}
+
+.search-btn:hover {
+    color: white;
+}
+
+.search-txt{
+    border: none;
+    background: none;
+    outline: none;
+    padding: 0;
+    color: #404040;
+    font-size: 16px;
+    transition: 0.4s;
+    line-height: 20px;
+    width: 650px;
+}
+
+.caret-down-btn{
+    color: #52c3a5;
+    float: right;
+    margin-right: 10px;
+}
+
+.caret-down-btn:hover {
+    color: white;
+}
+
+/*Home content styles*/
 #welcome-box {
+    margin-top: 0%;
     background-color: #b9e8d4;
     height: 14vw;
     border-radius: 12px;
